d2ffb2b7a2affaeb83bfe57216cf02ccd3466464
Dave Bellagio <dbellagio@us.ibm.com>
2023-03-20 15:44:43 -0500

    tested this refactor and everything came up ok

--- examples/satellite-gcp/outputs.tf
+++ examples/satellite-gcp/outputs.tf
@@ -10,19 +10,19 @@ output "gcp_storage_host_links" {
 output "gcp_worker_host_links" {
   value = [for host in google_compute_instance_from_template.gcp_worker_hosts : host.self_link]
 }
-output "gcp_control_plane_host_names" {
+output "gcp_control_plane_host_instance_ids" {
   value = flatten([for host in google_compute_instance_from_template.gcp_control_plane_hosts : host.instance_id])
 }
-output "gcp_storqge_host_names" {
+output "gcp_storage_host_instance_ids" {
   value = flatten([for host in google_compute_instance_from_template.gcp_storage_hosts : host.instance_id])
 }
-output "gcp_worker_host_names" {
+output "gcp_worker_host_instance_ids" {
   value = flatten([for host in google_compute_instance_from_template.gcp_worker_hosts : host.instance_id])
 }
 output "gcp_control_plane_host_ids" {
   value = flatten([for host in google_compute_instance_from_template.gcp_control_plane_hosts : host.id])
 }
-output "gcp_storqge_host_ids" {
+output "gcp_storage_host_ids" {
   value = flatten([for host in google_compute_instance_from_template.gcp_storage_hosts : host.id])
 }
 output "gcp_worker_host_ids" {
