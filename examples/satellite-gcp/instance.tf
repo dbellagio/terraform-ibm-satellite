715cf452becb23dc6d958035227ea3ee3de480c2
Dave Bellagio <dbellagio@us.ibm.com>
2023-03-21 18:20:30 -0500

    Remove files that are no longer needed due to the refactor to resources

--- examples/satellite-gcp/instance.tf
+++ examples/satellite-gcp/instance.tf
@@ -22,12 +22,6 @@ data "ibm_satellite_attach_host_script" "attach_script_control_plane" {
 
  # host_provider = var.host_provider
   custom_script = <<EOF
-  set +e
-  date=`date`
-  #-------------------------------------------------------------------------------------------------------------------------
-  # The custom script has been removed from this example, but should be put here to enable user login or address repo issues
-  #-------------------------------------------------------------------------------------------------------------------------
-  set -e
 EOF
 }
 
@@ -37,12 +31,6 @@ data "ibm_satellite_attach_host_script" "attach_script_storage" {
 
  # host_provider = var.host_provider
   custom_script = <<EOF
-  set +e
-  date=`date`
-  #-------------------------------------------------------------------------------------------------------------------------
-  # The custom script has been removed from this example, but should be put here to enable user login or address repo issues
-  #-------------------------------------------------------------------------------------------------------------------------
-  set -e
 EOF
 }
 
@@ -52,54 +40,8 @@ data "ibm_satellite_attach_host_script" "attach_script_worker" {
 
  # host_provider = var.host_provider
   custom_script = <<EOF
-  set +e
-  date=`date`
-  #-------------------------------------------------------------------------------------------------------------------------
-  # The custom script has been removed from this example, but should be put here to enable user login or address repo issues
-  #-------------------------------------------------------------------------------------------------------------------------
-  set -e
 EOF
 }
-
-/*
-module "gcp_host-template" {
-  #----------------------------------------------------------------------
-  # the hosts map is constructed in locals.tf and is used in many places
-  #----------------------------------------------------------------------
-  for_each   = local.hosts
-  source     = "terraform-google-modules/vm/google//modules/instance_template"
-  version    = "6.5.0"
-  project_id = var.gcp_project
-  network     = var.gcp_shared_network
-  subnetwork         = var.gcp_subnet
-  name_prefix        = "${var.gcp_resource_prefix}-template"
-  tags               = ["ibm-satellite", var.gcp_resource_prefix]
-  labels = {
-    ibm-satellite = var.gcp_resource_prefix
-  }
-  metadata = {
-    ssh-keys       = var.ssh_public_key != null ? "${var.gcp_ssh_user}:${var.ssh_public_key}" : tls_private_key.rsa_key.0.public_key_openssh
-#   startup-script = module.satellite-location.host_script
-    startup-script = file(each.value.attach_script)
-    serial-port-enable = true
-  }
-  # startup_script=module.satellite-location.host_script
-  machine_type         = each.value.instance_type
-  can_ip_forward       = false
-  source_image_project = var.worker_image_project
-  source_image_family  = var.worker_image_family
-  disk_size_gb         = 100
-  disk_type            = "pd-ssd"
-  disk_labels = {
-    ibm-satellite = var.gcp_resource_prefix
-  }
-  auto_delete     = true
-  service_account = { email = "", scopes = [] }
-  additional_disks     = each.value.additional_disks
-  depends_on      = [module.satellite-location]
-
-}
-*/
 
 resource "google_compute_instance_template" "gcp_control_plane_host_template" {
   for_each   = var.control_plane_hosts
@@ -335,34 +277,6 @@ resource "google_compute_instance_template" "gcp_storage_host_template" {
   }
 
 }
-
-/*
-module "gcp_hosts" {
-  #----------------------------------------------------------------------
-  # the hosts map is constructed in locals.tf and is used in many places
-  #----------------------------------------------------------------------
-  for_each           = local.hosts
-  source             = "terraform-google-modules/vm/google//modules/compute_instance"
-  version            = "7.9.0"
-  region             = var.gcp_region
-  network            = var.gcp_shared_network
-  subnetwork         = var.gcp_subnet
-  #--------------------------------------
-  # this is set to 1 in locals.tf
-  #--------------------------------------
-  num_instances      = each.value.count
-  hostname           = "${var.gcp_resource_prefix}-${each.key}"
-  instance_template  = module.gcp_host-template[each.key].self_link
-  #------------------------------------
-  # sepcify each zone sepcifically
-  #------------------------------------
-  zone               = each.value.zone
-  #---------------------------------
-  # Suppress GCP suffix on hostnames
-  #---------------------------------
-  add_hostname_suffix = false
-}
-*/
 
 resource "google_compute_instance_from_template" "gcp_control_plane_hosts" {
 
